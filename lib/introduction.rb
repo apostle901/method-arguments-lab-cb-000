e41f319c866903ed27a6946d1a02be0a920e631d
Jawann <jawann@gmail.com>
2017-11-26 06:38:37 +0000

    automatically backed up by learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction (name) do
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language (name1, name2) do
+  puts "Hi, my name is #{name1} and I am learning to program in #{name2}."
+end
